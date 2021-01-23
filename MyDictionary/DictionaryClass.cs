cced1a79ebd162ef3921f8c3ae1bcc88dd54e338
burak hüseyin güvenç <guvencburak39@gmail.com>
2021-01-23 16:38:44 +0300

    Add project files.

---
+++ MyDictionary/DictionaryClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class DictionaryClass<TProd,TPrice>
+    {
+        TProd[] Products;
+        TPrice[] Prices;
+
+        public DictionaryClass()
+        {
+            Products = new TProd[0];
+            Prices = new TPrice[0];
+
+        }
+        public void Add(TProd Product,TPrice Price)
+        {
+            TProd[] tempProducts = Products;
+            TPrice[] tempPrices = Prices;
+
+            Products = new TProd[Products.Length+1];
+            Prices = new TPrice[Prices.Length+1];
+
+            for (int i = 0; i < tempProducts.Length; i++)
+            {
+                Products[i] = tempProducts[i];
+                Prices[i] = tempPrices[i];
+
+                
+            }
+            Products[Products.Length - 1] = Product;
+            Prices[Prices.Length - 1] = Price;
+      
+        }
+        public int Count
+        {
+            get { return Products.Length; }
+
+
+        }
+
+
+
+    }
+        
+
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DictionaryClass<int, string> dictionaryClass = new DictionaryClass<int, string>();
+            dictionaryClass.Add(4500, "hp notebook");
+            dictionaryClass.Add(5600, "huawei notebook");
+
+            Console.WriteLine(dictionaryClass.Count);
+            
+    }
+
+    }
+}
